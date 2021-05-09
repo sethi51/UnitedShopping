333f12ab4c14270d5ad2eac2867426b1211f9455
Abhishek Sethi <abhisethi.53@gmail.com>
2021-05-09 19:13:23 +0530

    Init

---
+++ UnitedShopingBackEnd/src/main/java/com/united/UnitedShopingBackEndApplication.java
@@ -0,0 +1,14 @@
+package com.united;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitedShopingBackEndApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello From Console.");
+		SpringApplication.run(UnitedShopingBackEndApplication.class, args);
+	}
+
+}
---
+++ UnitedShopingBackEnd/src/main/java/com/united/model/User.java
@@ -0,0 +1,92 @@
+package com.united.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class User {
+	@Id
+	@SequenceGenerator(name = "user_id", initialValue = 10001, allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id")
+	private int userId;
+	
+	@Column
+	private String userFirstName;
+	
+	@Column
+	private String userLastName;
+	
+	@Column
+	private String userGender;
+	
+	@Column
+	private String userMobile;
+	
+	@Column
+	private String userMail;
+	
+	@Column
+	private String userPassword;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserFirstName() {
+		return userFirstName;
+	}
+
+	public void setUserFirstName(String userFirstName) {
+		this.userFirstName = userFirstName;
+	}
+
+	public String getUserLastName() {
+		return userLastName;
+	}
+
+	public void setUserLastName(String userLastName) {
+		this.userLastName = userLastName;
+	}
+
+	public String getUserGender() {
+		return userGender;
+	}
+
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+
+	public String getUserMobile() {
+		return userMobile;
+	}
+
+	public void setUserMobile(String userMobile) {
+		this.userMobile = userMobile;
+	}
+
+	public String getUserMail() {
+		return userMail;
+	}
+
+	public void setUserMail(String userMail) {
+		this.userMail = userMail;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	
+	
+}
